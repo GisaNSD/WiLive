0291804d72ed1f660b68f1413f3dd1f0722484c3
Laura <laura.rodcarrrion@gmail.com>
2021-01-14 15:10:50 +0100

    landing page skelleton

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WiLive</title>
-    <link rel="stylesheet" href="/resources/css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-<img src="https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59bbb29c5bafe878503c9872/husky-siberiano-bosque.jpg" alt="">
+<header>
 
+<img src="./imagenes/Logo.png" alt="fondo">
+</header>
 
+<!-- BANNER -->
+<section class="banner">
+    <img src="" alt="">
+    <h1>BIENVENID@ A LA VIDA<br> ONLINE</h1>
+    <p>Lorem ipsum dolor sit amer, consectetur adipiscing elit.<br> Sed varius risus maecenas integer neque</p>
+</section>
 
-    
+<h2>QUE QUIERES HACER?</h2>
+
+<ul class="options">
+    <li>APRENDE</li>
+    <li>DEBATE</li>
+    <li>DESCUBRE</li>
+    <li>VIAJA</li>
+    <li>COME</li>
+    <li>AGENDA</li>
+</ul>
+
+<button id="help">TE AYUDAMOS</button>
+
+<footer>
+Design with LOVE by echoteams
+</footer>
 </body>
 </html>
