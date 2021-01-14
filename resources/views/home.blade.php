780b8bba0cf3d398853fcdad0318cb1f665df889
Laura <laura.rodcarrrion@gmail.com>
2021-01-14 14:09:42 +0100

    logo fail

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WiLive</title>
+    <link rel="stylesheet" href="/resources/css/app.css">
+</head>
+<body>
+<img src="https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59bbb29c5bafe878503c9872/husky-siberiano-bosque.jpg" alt="">
+
+
+
+    
+</body>
+</html>
