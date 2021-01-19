3c986390013e12667556fe358866e9c9abcaefd5
Moi <mmena4@hotmail.com>
2021-01-19 12:09:30 +0100

    first commit wilive-main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WiLive</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<header>
+
+<img src="./imagenes/Logo.png" alt="fondo">
+</header>
+
+<!-- BANNER -->
+<section class="banner">
+    <img src="" alt="">
+    <h1>BIENVENID@ A LA VIDA<br> ONLINE</h1>
+    <p>Lorem ipsum dolor sit amer, consectetur adipiscing elit.<br> Sed varius risus maecenas integer neque</p>
+</section>
+
+<h2>QUE QUIERES HACER?</h2>
+
+<ul class="options">
+    <li>APRENDE</li>
+    <li>DEBATE</li>
+    <li>DESCUBRE</li>
+    <li>VIAJA</li>
+    <li>COME</li>
+    <li>AGENDA</li>
+</ul>
+
+<button id="help">TE AYUDAMOS</button>
+
+<footer>
+Design with LOVE by echoteams
+</footer>
+</body>
+</html>
