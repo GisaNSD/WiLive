8c8e77289949332b6771bac948798abd3125724f
Giselle <gisi_tina4@hotmail.com>
2021-01-19 14:22:29 +0100

    Css in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,25 +7,33 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/app.css">
+    <script src="https://kit.fontawesome.com/b1ccc03917.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
 <header>
-    <img src="/imagenes/Logo.png" alt="fondo">
+
+    <img class="logo" src="/imagenes/Logo.png" alt="fondo">
+    <p>Desliza para ver el contenido</p>
+    <i class="fas fa-arrow-circle-down"></i>
 </header>
 
-<h2>QUE QUIERES HACER?</h2>
+<section>
+    <h2>¿Qué quieres hacer?</h2>
 
-<ul class="options">
-    <li>APRENDE</li>
-    <li>DEBATE</li>
-    <li>VIAJA</li>
-    <li>COME</li>
-</ul>
+    <ul class="options">
+        <li>APRENDE</li>
+        <li>VIAJA</li>
+        <li>DEBATE</li>
+        <li>COME</li>
+    </ul>
+
+</section>
 
 <button id="help">TE AYUDAMOS</button>
-
 <footer>
 Design with LOVE by echoteams
 </footer>
+
 </body>
 </html>
