d562df01f4a0a2a285a58d06066536a3fc231503
JosephCrespin <67466171+JosephCrespin@users.noreply.github.com>
2021-01-29 13:41:17 +0100

    register, log done, pero falta la ruta ayuda

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
+
+    public function index()
+    {
+        $users = User::all();
+
+        return view('admin-dashboard', ['users' => $users]);
+    }
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('edit-user', ['user' => $user]);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('show-user', ['user' => $user]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $user->name = $request->name;
+        $user->isAdmin = $request->isAdmin;
+
+        $user->save();
+
+        return view('show-user', ['user' => $user]);
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+     use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'isAdmin',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 
     public function event()
     {
